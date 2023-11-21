bbd57b2580f10a7832a0788ce27b9d399ede1655
laertLAW <143403309+laertLAW@users.noreply.github.com>
2023-11-21 09:40:52 -0300

    atividade ate a 20

--- lista-2/at15.py
+++ lista-2/at15.py
@@ -8,4 +8,4 @@ if ((l1 + l2 > l3) and (l1 + l3 > l2) and (l2 + l3 > l1)):
     elif l1 == l2 or l1 == l3 or l2 == l3:
         print("É um triângulo isoceles")
     else:
-        
+        print("Inválido")
